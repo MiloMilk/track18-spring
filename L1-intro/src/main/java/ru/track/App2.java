769ceb336ace40cb5760e443cd15ea07d07d235e
MiloMilk <nyno4ek.olo@gmail.com>
2018-02-27 17:07:28 +0300

    App2 editing

--- L1-intro/src/main/java/ru/track/App2.java
+++ L1-intro/src/main/java/ru/track/App2.java
@@ -1,12 +1,24 @@
 package ru.track;
 
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.apache.commons.lang3.StringUtils;
 
 public class App2 {
 
-    public static void main(String[] args) {
-        String str = StringUtils.capitalize(args[0]);
-        System.out.println(str);
+    public static void main(String[] args) throws UnirestException {
+        HttpResponse<JsonNode> r = Unirest.post("https://guarded-mesa-31536.herokuapp.com/track")
+                .header("accept", "application/json")
+                .field("name", "Иван")
+                .field("github", "https://github.com/MiloMilk")
+                .field("email", "nyno4ek.olo@gmail.com")
+                .asJson();
+
+
+        r.getBody().getObject().get("success");
+// Должен венруть true
     }
 }
